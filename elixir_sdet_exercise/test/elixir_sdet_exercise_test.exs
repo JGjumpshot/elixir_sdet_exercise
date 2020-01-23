64909a770fde65ce98a5b556ead51dcde5ec6e98
Jackson Gunther <jackson.gunther@yahoo.com>
2020-01-22 18:53:55 -0700

    signup less than 16 complete

--- elixir_sdet_exercise/test/elixir_sdet_exercise_test.exs
+++ elixir_sdet_exercise/test/elixir_sdet_exercise_test.exs
@@ -2,7 +2,7 @@ defmodule ElixirSdetExerciseTest do
   # Import helpers
   use Hound.Helpers
   use ExUnit.Case, async: false
- 
+
   # Start hound session and destroy when tests are run
   hound_session()
 
@@ -136,7 +136,7 @@ defmodule ElixirSdetExerciseTest do
     click(select_gender)
     sign_up_button = find_element(:name, "websubmit")
     click(sign_up_button)
-    
+
     element = find_element(:css, "#reg_error")
     assert element !== nil
   end
@@ -176,7 +176,7 @@ defmodule ElixirSdetExerciseTest do
     # # found = element_displayed?("#reg_error_inner")
     # IO.puts(search)
     # # assert found == true
-    :timer.sleep(10000)
+    # :timer.sleep(10000)
   end
 
   @tag runnable: false
@@ -207,45 +207,79 @@ defmodule ElixirSdetExerciseTest do
     click(select_gender)
     sign_up_button = find_element(:name, "websubmit")
     click(sign_up_button)
-    
+
     element = find_element(:css, "#reg_error_inner")
     assert element !== nil
 
-    :timer.sleep(10000)
+    # :timer.sleep(10000)
   end
 
-  # @tag runnable: true
-  # test "Email without @ symbol" do
-  #   counter = :rand.uniform(100)
-  #   navigate_to "https://www.facebook.com"
-  #   first_name = find_element(:name, "firstname")
-  #   fill_field(first_name, "Fred")
-  #   last_name = find_element(:name, "lastname")
-  #   fill_field(last_name, "Vandenberg")
-  #   email_input = "Fred#{counter}gmail.com"
-  #   email = find_element(:name, "reg_email__")
-  #   fill_field(email, email_input)
-  #   confirm_email = find_element(:name, "reg_email_confirmation__")
-  #   fill_field(confirm_email, email_input)
-  #   password_input = "111222"
-  #   password = find_element(:name, "reg_passwd__")
-  #   fill_field(password, password_input)
-  #   #Birthday
-  #   birth_month = find_element(:css, "#month > option:nth-child(5)")
-  #   birth_day = find_element(:css, "#day > option:nth-child(25)")
-  #   birth_year = find_element(:css, "#year > option:nth-child(22)")
-  #   click(birth_month)
-  #   click(birth_day)
-  #   click(birth_year)
-  #   #gender
-  #   select_gender = find_element(:css, "#u_0_s > span:nth-child(2)")
-  #   click(select_gender)
-  #   sign_up_button = find_element(:name, "websubmit")
-  #   click(sign_up_button)
-    
-  #   element = find_element(:css, "#reg_error_inner")
-  #   assert element !== nil
+  @tag runnable: false
+  test "Email without @ symbol" do
+    counter = :rand.uniform(100)
+    navigate_to "https://www.facebook.com"
+    first_name = find_element(:name, "firstname")
+    fill_field(first_name, "Fred")
+    last_name = find_element(:name, "lastname")
+    fill_field(last_name, "Vandenberg")
+    email_input = "FredV#{counter}gmail.com"
+    email = find_element(:name, "reg_email__")
+    fill_field(email, email_input)
+    password_input = "111222T@"
+    password = find_element(:name, "reg_passwd__")
+    fill_field(password, password_input)
+    #Birthday
+    birth_month = find_element(:css, "#month > option:nth-child(5)")
+    birth_day = find_element(:css, "#day > option:nth-child(25)")
+    birth_year = find_element(:css, "#year > option:nth-child(22)")
+    click(birth_month)
+    click(birth_day)
+    click(birth_year)
+    #gender
+    select_gender = find_element(:css, "#u_0_s > span:nth-child(2)")
+    click(select_gender)
+    sign_up_button = find_element(:name, "websubmit")
+    click(sign_up_button)
 
-  #   :timer.sleep(10000)
-  # end
+    pop_up = find_element(:css, "#globalContainer > div.uiContextualLayerPositioner._572t.uiLayer > div > div")
+    text = visible_text(pop_up)
+    assert String.length(text) == 75
+
+    # :timer.sleep(10000)
+  end
+
+  @tag runnable: true
+  test "sign-up is less than 16 years" do
+    counter = :rand.uniform(100)
+    navigate_to "https://www.facebook.com"
+    first_name = find_element(:name, "firstname")
+    fill_field(first_name, "Fred")
+    last_name = find_element(:name, "lastname")
+    fill_field(last_name, "Vandenberg")
+    email_input = "FredV#{counter}@gmail.com"
+    email = find_element(:name, "reg_email__")
+    fill_field(email, email_input)
+    confirm_email = find_element(:name, "reg_email_confirmation__")
+    fill_field(confirm_email, email_input)
+    password_input = "111222T@"
+    password = find_element(:name, "reg_passwd__")
+    fill_field(password, password_input)
+    #Birthday
+    birth_month = find_element(:css, "#month > option:nth-child(2)")
+    birth_day = find_element(:css, "#day > option:nth-child(11)")
+    birth_year = find_element(:css, "#year > option:nth-child(10)")
+    click(birth_month)
+    click(birth_day)
+    click(birth_year)
+    #gender
+    select_gender = find_element(:css, "#u_0_s > span:nth-child(2)")
+    click(select_gender)
+    sign_up_button = find_element(:name, "websubmit")
+    click(sign_up_button)
+    :timer.sleep(1000)
+    pop_up = find_element(:css, ".fsl.fwb.fcb")
+    text = visible_text(pop_up)
+    #IO.puts(String.length(text))
+    assert String.length(text) == 52
+  end
 end
